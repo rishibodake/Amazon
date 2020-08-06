9cb2b3c02b096383c0d2982906bf1b40543931d7
abhib <rishibodake@gmail.com>
2020-08-06 17:45:18 +0530

    Added:The Report Generation Feature To Generate The Report For Test Cases

--- AmazonProject/TestCases/NegativeTestScenarios.cs
+++ AmazonProject/TestCases/NegativeTestScenarios.cs
@@ -1,4 +1,6 @@
 ﻿using AmazonProject.Pages;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using NUnit.Framework;
 using OpenQA.Selenium;
 
@@ -8,22 +10,40 @@ namespace AmazonProject.TestCases
     [Parallelizable]
     public class NegativeTestScenarios : BaseTest
     {
+        ExtentReports report = null;
+
+        [OneTimeSetUp]
+        public void ExtendStart()
+        {
+            report = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\abhib\source\repos\AmazonProject\AmazonProject\Reports\NegativeScenariosReport\");
+            report.AttachReporter(htmlReporter);
+        }
+
         [Test]
         public void Incorrect_Email_Test()
         {
+            ExtentTest test = report.CreateTest("Incorrect_Email_Test").Info("Test For Incorrect Email");
             DoInvalidActions.Incorrect_Email(driver);
             string check = driver.FindElement(By.ClassName("a-alert-heading")).Text;
             Assert.AreEqual(check, "Incorrect phone number");
-
-
+            test.Log(Status.Pass, "Sign In Failed, Test Succesful");
         }
 
         [Test]
         public void Incorrect_Password_Test()
         {
+            ExtentTest test = report.CreateTest("Incorrect_Password_Test").Info("Test For Incorrect Password");
             DoInvalidActions.Incorrect_Password(driver);
             string check = driver.FindElement(By.ClassName("a-list-item")).Text;
             Assert.AreEqual(check, "Your password is incorrect");
+            test.Log(Status.Pass, "Test Passed");
+        }
+
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            report.Flush();
         }
     }
 }
--- AmazonProject/TestCases/TestClass.cs
+++ AmazonProject/TestCases/TestClass.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
 
 namespace AmazonProject
 {
@@ -6,26 +8,47 @@ namespace AmazonProject
     [Parallelizable]
     public class TestClass : BaseTest
     {
+
+        ExtentReports report = null;
+
+        [OneTimeSetUp]
+        public void ExtendStart()
+        {
+            report = new ExtentReports();
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\abhib\source\repos\AmazonProject\AmazonProject\Reports\PositiveScenariosReport\");
+            report.AttachReporter(htmlReporter);
+        }
+
         [Test,Order(0)]
         public void SignIn_Test()
         {
-            DoValidAction.SignIn(driver);           
+            ExtentTest test = report.CreateTest("SignIn_Test").Info("Test To Sign In into Application");
+            DoValidAction.SignIn(driver);
             Assert.AreEqual(Validation.DoValidation(driver,"signin validation"), "Hello, Rishi");
+            test.Log(Status.Pass, "Sign In Succesful");
         }
 
       
         [Test,Order(1)]
         public void Search_Test()
         {
+            ExtentTest test = report.CreateTest("Search_Test").Info("Test To Search The Product");
             DoValidAction.SerachProduct(driver);
+            test.Log(Status.Pass, "Search Succesfull");
         }
 
         [Test,Order(2)]
         public void SignOut_Test()
         {
-           DoValidAction.SignOut(driver);
+            ExtentTest test = report.CreateTest("SignOut_Test").Info("Test To Sign Out from Application");
+            DoValidAction.SignOut(driver);
+            test.Log(Status.Pass, "Sign Out  Succesfull");
         }
 
-       
+        [OneTimeTearDown]
+        public void ExtentClose()
+        {
+            report.Flush();         
+        }
     }
 }
